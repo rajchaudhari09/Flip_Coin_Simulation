305bc8a7379711ffd70d56b02cbd7ebee2ebec1a
Vishal Rajput <vishalpatil01@hotmail.com>
2020-01-24 18:10:17 +0530

    ADDED :FlipCoin for 2 Coins

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,30 +1,76 @@
 #!/bin/bash 
+
 echo "Welcome To Flip Coin Simulator"
 
-#!Declaring Varibles
+function simulator(){
+
+#!Declaring Variables
+key=""
+flips=$1
+coins=$2
+
+#!declaring Dictionary
 declare -A coinFacesDict
-H=0
-T=0
 
-#!function for Flip Coin
-function flipCoin() {
-read -p "Enter How Many Times You want To FlipCoin : " flips
-
-for((i=0;i<flips;i++))
+for((i=1;i<=$flips;i++))
 do
-	if(($((RANDOM % 2)) == 1 ))
-	then
-		coinFacesDict["result"$i]="Head"
-		((H++))
-	else
-		coinFacesDict["result"$i]="Tails"
-			((T++))
-	fi
+	for((j=1;j<=$coins;j++))
+	do
+		if(( $((RANDOM%2==0)) ))
+		then
+ 			key+=H
+		else
+ 			key+=T
+		fi
+	done
+	coinFacesDict[$key]=$((${coinFacesDict[$key]}+1))
+	key=""
 done
-echo "Heads Percentage = $(echo "scale=2; $H * 100 / $flips" | bc )"
-echo "Tails Percentage =  $(echo "scale=2; $T * 100 / $flips" | bc )"
+
+echo "All Occurrence : " ${!coinFacesDict[@]}
+echo "All Occurrence : " ${coinFacesDict[@]}
+
+#!calling calulate Percentage Function
+calcPercentage
 
 }
-flipCoin
-echo "All Occurrence : "${coinFacesDict[@]}
-#!Calling Function
+
+function calcPercentage() {
+for keys in ${!coinFacesDict[@]}
+do
+		coinFacesDict[$keys]=$((${coinFacesDict[$keys]} * 100 / $flips))
+done
+
+echo "All Occurrence key : " ${!coinFacesDict[@]}
+echo "All Occurrence Percentage: " ${coinFacesDict[@]}
+
+}
+
+#!calling function
+function startGame()
+{
+	local flips=0
+	read -p "Press y to play other key for Quite : " play
+	while [ $play == 'y' ]
+	do
+		read -p "Enter How Many Times You want To FlipCoin : " flips
+		echo -e "Enter 1 for Single coin \nEnter 2 for Two Coins "
+		read choice
+		case $choice in
+			1)
+				simulator $flips 1
+				;;
+			2)
+				simulator $flips 2
+				;;
+			*)
+				echo "Invalid Option"
+				;;
+		esac
+	read -p "Press 1 to play other key for Quite : " play
+	done
+}
+
+#!Starting Program
+startGame
+
