c1e825e4f210ac2addabb8693c19f04b90068856
Vishal Rajput <vishalpatil01@hotmail.com>
2020-01-24 10:59:18 +0530

    ADDED : FlipCoin Function

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,2 +1,16 @@
 #!/bin/bash -x
 echo "Welcome To Flip Coin Simulator"
+
+#!function for Flip Coin
+
+function flipCoin() {
+if(($((RANDOM % 2)) == 1 ))
+then
+	echo "Heads"
+else
+	echo "Tails"
+fi
+}
+
+#!Calling Function
+flipCoin
