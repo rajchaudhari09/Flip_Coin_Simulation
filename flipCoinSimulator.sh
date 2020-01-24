1c74b38d09597dd88438d0c05ed9c0850b2b256a
Vishal Rajput <vishalpatil01@hotmail.com>
2020-01-24 12:18:53 +0530

    ADDED : FlipCoin Percentage Calculation

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,16 +1,30 @@
-#!/bin/bash -x
+#!/bin/bash 
 echo "Welcome To Flip Coin Simulator"
 
+#!Declaring Varibles
+declare -A coinFacesDict
+H=0
+T=0
+
 #!function for Flip Coin
-
 function flipCoin() {
-if(($((RANDOM % 2)) == 1 ))
-then
-	echo "Heads"
-else
-	echo "Tails"
-fi
-}
+read -p "Enter How Many Times You want To FlipCoin : " flips
 
-#!Calling Function
+for((i=0;i<flips;i++))
+do
+	if(($((RANDOM % 2)) == 1 ))
+	then
+		coinFacesDict["result"$i]="Head"
+		((H++))
+	else
+		coinFacesDict["result"$i]="Tails"
+			((T++))
+	fi
+done
+echo "Heads Percentage = $(echo "scale=2; $H * 100 / $flips" | bc )"
+echo "Tails Percentage =  $(echo "scale=2; $T * 100 / $flips" | bc )"
+
+}
 flipCoin
+echo "All Occurrence : "${coinFacesDict[@]}
+#!Calling Function
